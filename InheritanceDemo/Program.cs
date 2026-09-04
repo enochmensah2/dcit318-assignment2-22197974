4f040004192a5ee0ee8a35a6c031b5ae582b2d3c
Enock Mensah <sonofafrica14@gmail.com>
2026-09-04 23:49:34 +0000

    Add Inheritance demo

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace InheritanceDemo
+{
+    
+    class Animal
+    {
+        
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    
+    class Dog : Animal
+    {
+        
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    
+    class Cat : Animal
+    {
+        
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Inheritance & Method Overriding ---");
+
+            
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            
+            Console.Write("Animal says: ");
+            genericAnimal.MakeSound();
+
+            Console.Write("Dog says: ");
+            dog.MakeSound();
+
+            Console.Write("Cat says: ");
+            cat.MakeSound();
+
+            Console.ReadKey(); 
+        }
+    }
+}
