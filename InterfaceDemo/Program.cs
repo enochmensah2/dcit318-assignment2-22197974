d6a30ca2bc2ddaa03b9c8af089a75b749b039625
Enock Mensah <sonofafrica14@gmail.com>
2026-09-04 23:50:51 +0000

    Add Interface demo

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    
+    interface IMovable
+    {
+        void Move(); 
+    }
+
+    
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Interfaces ---");
+
+            
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            
+            Console.Write("Car: ");
+            car.Move();
+
+            Console.Write("Bicycle: ");
+            bicycle.Move();
+
+            Console.ReadKey();
+        }
+    }
+}
